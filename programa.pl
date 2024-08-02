13517f3234691d3790c3ee4347be3ea1650e8824
Lucia Garcia <lucigarcia@frba.utn.edu.ar>
2024-08-02 18:38:06 -0300

    terminado

--- programa.pl
+++ programa.pl
@@ -1,32 +1,24 @@
 % TP Lógico Eish of PDEP - Grupo Vmsl - 1era entrega
 % Civilizaciones y tecnologías
-
-
 % 1) 
 unJugador(ana).
 unJugador(beto).
 unJugador(carola).
 unJugador(dimitri).
-
 juegaCon(ana,romanos).
 juegaCon(beto,incas).
 juegaCon(carola,romanos).
 juegaCon(dimitri,romanos).
-
 desarrolloTecnologia(ana,herreria).
 desarrolloTecnologia(ana,forja).
 desarrolloTecnologia(ana,emplumado).
 desarrolloTecnologia(ana,laminas).
-
 desarrolloTecnologia(beto,herreria).
 desarrolloTecnologia(beto,forja).
 desarrolloTecnologia(beto,fundicion).
-
 desarrolloTecnologia(carola,herreria).
-
 desarrolloTecnologia(dimitri,herreria).
 desarrolloTecnologia(dimitri,fundicion).
-
 % 2) 
 /*
 Saber si un jugador es experto en metales, que sucede cuando desarrolló las tecnologías de herrería, forja y o bien desarrolló fundición o bien juega con los romanos.
@@ -35,11 +27,9 @@ En los ejemplos, Ana y Beto son expertos en metales, pero Carola y Dimitri no.
 expertoEnMetales(Jugador):-
     condicionExperto(Jugador),
     desarrolloTecnologia(Jugador,fundicion).
-
 expertoEnMetales(Jugador):-
     condicionExperto(Jugador),
     juegaCon(Jugador,romanos).
-
 condicionExperto(Jugador):-
     desarrolloTecnologia(Jugador,herreria),
     desarrolloTecnologia(Jugador,forja).
@@ -48,13 +38,10 @@ condicionExperto(Jugador):-
 Saber si una civilización es popular, que se cumple cuando la eligen varios jugadores (más de uno).
 En los ejemplos, los romanos son una civilización popular, pero los incas no.
 */
-
-
 civilizacionPopular(Civilizacion):-
     juegaCon(Jugador1,Civilizacion),
     juegaCon(Jugador2,Civilizacion),
     Jugador1 \= Jugador2.
-
 % 4) 
 /*
 Saber si una tecnología tiene alcance global, que sucede cuando a nadie le falta desarrollarla.
@@ -76,27 +63,23 @@ unaTecnologia(molino).
 unaTecnologia(collera).
 unaTecnologia(arado).
 
-
 tieneAlcanceGlobal(Tecnologia):-
     unaTecnologia(Tecnologia),
     forall(unJugador(Jugador), desarrolloTecnologia(Jugador, Tecnologia)).
-
-
 % 5) 
 /*
 Saber cuándo una civilización es líder. Se cumple cuando esa civilización alcanzó todas las tecnologías que alcanzaron las demás. Una civilización alcanzó una tecnología cuando algún jugador de esa civilización la desarrolló.
 En los ejemplos, los romanos son una civilización líder pues entre Ana y Dimitri, que juegan con romanos, ya tienen todas las tecnologías que se alcanzaron.
 */
-
 civilizacionAlcanzoTecnologia(Civilizacion,Tecnologia):-
     unJugador(Jugador),
     juegaCon(Jugador,Civilizacion),
     desarrolloTecnologia(Jugador,Tecnologia).
 
 esLider(Civilizacion):-
-<<<<<<< HEAD
-    civilizacion(Civilizacion),
-    forall(unaTecnologia(Tecnologia), civilizacionAlcanzoTecnologia(Civilizacion, Tecnologia)).
+    juegaCon(_,Civilizacion),
+    forall(civilizacionAlcanzoTecnologia(Civilizacion2, Tecnologia),civilizacionAlcanzoTecnologia(Civilizacion, Tecnologia)).
+
 
 % 6)
 /*Modelar lo necesario para representar las distintas unidades de cada jugador de la forma más conveniente 
@@ -181,7 +164,7 @@ cuenta_piqueros(Jugador, Tipo, Cantidad) :-
 sobreviveAsedio(Jugador):-
     unJugador(Jugador),
     cuenta_piqueros(Jugador, escudo, CantConEscudo),
-    cuenta_piqueros(Jugador, escudo, CantSinEscudo),
+    cuenta_piqueros(Jugador, sinEscudo, CantSinEscudo),
     CantConEscudo > CantSinEscudo.
 
 
@@ -223,7 +206,4 @@ puedeDesarrollar(Jugador, Tecnologia):-
     not(desarrolloTecnologia(Jugador, Tecnologia)),
     findall(T, (unaTecnologia(T), dependenciaDirecta(Tecnologia, T)), Lista),
     forall((unaTecnologia(X), member(X, Lista)), desarrolloTecnologia(Jugador, X)).
-=======
-    juegaCon(_,Civilizacion),
-    forall(civilizacionAlcanzoTecnologia(Civilizacion2, Tecnologia),civilizacionAlcanzoTecnologia(Civilizacion, Tecnologia)).
->>>>>>> 1e28fec31dd80ac7dad99ced67dce36c25f40fee
+
